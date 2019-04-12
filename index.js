43e9896cf48372f5f4dd71b6d99008a17b9a6cb5
Arcane Disgea <magicdisgea@gmail.com>
2019-04-12 16:57:56 -0500

    go back in time

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const config = require('config')
-
-const prefix = config.get('prefix')
+const { prefix, token } = require("./config.json");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -88,4 +86,4 @@ client.on("message", message => {
   }
 });
 
-client.login(config.get('token'))
+client.login(token);
