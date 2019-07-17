55fa287f3c0ee30a2ae841fc713a75108800b43e
ArcaneDisgea <39720490+ArcaneDisgea@users.noreply.github.com>
2019-07-17 11:28:46 -0400

    fix

--- commands/attacked.js
+++ commands/attacked.js
@@ -6,7 +6,7 @@ module.exports = {
   description: "I feel attacked!",
   execute(message, args) {
     // Get the buffer from the 'file', assuming that the file exists
-    const buffer = fs.readFileSync("https://media1.tenor.com/images/b07b174865379ab42f1bd5599d490d38/tenor.gif");
+    const buffer = fs.readFileSync("./assets/images/tenor.gif");
     const attachment = new Attachment(buffer, "tenor.gif");
     // Send the attachment in the message channel with a content
     message.channel.send(attachment);
