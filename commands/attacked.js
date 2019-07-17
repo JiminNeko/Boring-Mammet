65f6b9e97f872ec1b9518cef62db19e42d9ae62e
ArcaneDisgea <MagicDisgea@gmail.com>
2019-07-17 12:39:35 -0400

    update to new name

--- commands/attacked.js
+++ commands/attacked.js
@@ -6,8 +6,8 @@ module.exports = {
   description: "I feel attacked!",
   execute(message, args) {
     // Get the buffer from the 'file', assuming that the file exists
-    const buffer = fs.readFileSync("./assets/images/tenor.gif");
-    const attachment = new Attachment(buffer, "tenor.gif");
+    const buffer = fs.readFileSync("./assets/images/attacked.gif");
+    const attachment = new Attachment(buffer, "attacked.gif");
     // Send the attachment in the message channel with a content
     message.channel.send(attachment);
   }
