9d748af89e95376d29c0b79b7c559d1042dea5b3
ArcaneDisgea <MagicDisgea@gmail.com>
2019-07-17 17:46:25 -0400

    fix the bruh moment

--- commands/bruh.js
+++ commands/bruh.js
@@ -6,7 +6,7 @@ module.exports = {
   description: "This really do be a bruhmoment.",
   execute(message, args) {
     // Get the buffer from the 'file', assuming that the file exists
-    const buffer = fs.readFileSync("./assets/images/bruht.gif");
+    const buffer = fs.readFileSync("./assets/images/bruh.gif");
     const attachment = new Attachment(buffer, "bruh.gif");
     // Send the attachment in the message channel with a content
     message.channel.send(attachment);
