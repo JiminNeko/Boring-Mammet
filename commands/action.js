8f9a3341acf21c2faf4bfd65c4067b46bee46b9e
Michael Shaw <shawm@hsd.nh.us>
2019-07-29 11:54:06 -0400

    final fix?

--- commands/action.js
+++ commands/action.js
@@ -1,26 +1,7 @@
 const { RichEmbed } = require("discord.js");
-
 const fetch = require("node-fetch");
+
 const xivsearch = "https://xivapi.com/search?string=";
-fetch(
-  xivsearch +
-    args +
-    `&string_algo=match&limit=1&indexes=Action,CraftAction&filters=IsPlayerAction=1,IsPvP=0&columns=ID,Name,Icon,ClassJobCategory.Name,Description,ActionCategory,ClassJob.Icon,IsPlayerAction`
-)
-  .then(resp => resp.json())
-  .then(function(search) {
-    let action = search.Results;
-    return action.map(function(actions) {
-      const embed = new RichEmbed()
-        .setAuthor(`${actions.Name}`, ``, `https://xivapi.com`)
-        .setDescription(`${actions.Description}`)
-        .setThumbnail(`${actions.Icon}`)
-        .setFooter("brought to you by xivapi.com");
-    });
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
 
 module.exports = {
   name: "action",
@@ -28,6 +9,25 @@ module.exports = {
   guildOnly: true,
   args: true,
   execute(message, args) {
+    fetch(
+      xivsearch +
+        args +
+        `&string_algo=match&limit=1&indexes=Action,CraftAction&filters=IsPlayerAction=1,IsPvP=0&columns=ID,Name,Icon,ClassJobCategory.Name,Description,ActionCategory,ClassJob.Icon,IsPlayerAction`
+    )
+      .then(resp => resp.json())
+      .then(function(search) {
+        let action = search.Results;
+        return action.map(function(actions) {
+          const embed = new RichEmbed()
+            .setAuthor(`${actions.Name}`, ``, `https://xivapi.com`)
+            .setDescription(`${actions.Description}`)
+            .setThumbnail(`${actions.Icon}`)
+            .setFooter("brought to you by xivapi.com");
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     message.channel.send(embed);
   }
 };
