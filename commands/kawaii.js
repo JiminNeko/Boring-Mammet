3d124f9e70b62d2b2c9b64b1d2ceddae3766b8c5
ArcaneDisgea <MagicDisgea@gmail.com>
2019-08-16 12:28:02 -0400

    fix

--- commands/kawaii.js
+++ commands/kawaii.js
@@ -5,8 +5,8 @@ module.exports = {
   description: " kawaii",
   execute(message, args) {
     // Get the buffer from the 'file', assuming that the file exists
-    const buffer = fs.readFileSync("./assets/images/kawaiiasian.png");
-    const attachment = new Attachment(buffer, "kawaiiasian.png");
+    const buffer = fs.readFileSync("./assets/images/kawaiiasian.gif");
+    const attachment = new Attachment(buffer, "kawaiiasian.gif");
     // Send the attachment in the message channel with a content
     message.channel.send(attachment);
   }
